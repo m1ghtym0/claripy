627ff494074a33c56c153c2fce326d9f079f931f
Moritz Eckert <mo@mightym0.de>
2017-09-14 11:23:16 -0700

    Add size_bytes

--- claripy/ast/bits.py
+++ claripy/ast/bits.py
@@ -5,15 +5,21 @@ class Bits(Base):
     A base class for AST types that can be stored as a series of bits. Currently, this is bitvectors and IEEE floats.
 
     :ivar length:       The length of this value in bits.
+    :ivar byte_size:    The number of bits in a byte.
     """
-    __slots__ = ['length']
+    __slots__ = ['length', 'byte_size']
 
     def __init__(self, *args, **kwargs):
         length = kwargs.pop('length', None)
         if length is None:
             raise ClaripyOperationError("length of Bits must not be None")
 
+        byte_size = kwargs.pop('byte_size', None)
+        if byte_size is None:
+            byte_size = 8
+
         self.length = length
+        self.byte_size = byte_size
 
     def make_like(self, *args, **kwargs):
         if 'length' not in kwargs: kwargs['length'] = self.length
@@ -21,6 +27,9 @@ class Bits(Base):
 
     def size(self):
         return self.length
+
+    def size_bytes(self):
+        return self.length/self.byte_size
 
     def _type_name(self):
         return self.__class__.__name__ + str(self.length)
